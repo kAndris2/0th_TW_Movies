3987dc10a5e06f47c60b471d81556eaec0daa17d
Nagy Krisztián <kicsikrissz111@gmail.com>
2019-11-27 14:46:51 +0100

    datamanager

---
+++ main/main/DataManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    class DataManager
+    {
+        public Dictionary<string, Dictionary<string, string>> Import_Data(String filename)
+        {
+            String[] table = System.IO.File.ReadAllLines(filename);
+            var mydict = new Dictionary<string, Dictionary<string, string>>();
+            string lastMovieTitle = "";
+            foreach (string lines in table)
+            {
+                if (lines.Contains("["))
+                {
+                    mydict[lines] = new Dictionary<string, string>();
+                    lastMovieTitle = lines;
+                }
+                if (lines.Contains("="))
+                {
+                    string[] parts = lines.Split('=');
+                    mydict[lastMovieTitle][parts[0]] = parts[1];
+                }
+            }
+            return mydict;
+        }
+    }
+}
--- main/main/Program.cs
+++ main/main/Program.cs
@@ -75,29 +75,5 @@ namespace main
             }
         }
     }
-
-    class DataManager
-    {
-        public Dictionary<string, Dictionary<string,string>> Import_Data(String filename)
-        {
-            String[] table = System.IO.File.ReadAllLines(filename);
-            var mydict = new Dictionary<string, Dictionary<string, string>>();
-            string lastMovieTitle = "";
-            foreach (string lines in table)
-            {
-                if (lines.Contains("["))
-                {
-                    mydict[lines] = new Dictionary<string, string>();
-                    lastMovieTitle = lines;
-                }
-                if (lines.Contains("="))
-                {
-                    string[] parts = lines.Split('=');
-                    mydict[lastMovieTitle][parts[0]] = parts[1];
-                }
-            }
-            return mydict;
-        }
-    }
 }
 
