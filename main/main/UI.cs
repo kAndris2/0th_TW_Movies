2f6b446cb513302e1782016f108e47bb4adf65ac
Andris <k.andris258@gmail.com>
2019-11-28 14:23:24 +0100

    UI added

---
+++ main/main/UI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace main
+{
+    public class UI
+    {
+        public UI(string message, List<string> result)
+        {
+            Console.WriteLine($"{message}:\n");
+            for (int i = 0; i < result.Count; i++)
+                Console.WriteLine(result[i]);
+        }
+
+        public UI(string message, string result)
+        {
+            Console.WriteLine($"{message}:\n");
+            Console.WriteLine(result);
+        }
+
+        public UI(string message, int result)
+        {
+            Console.WriteLine($"{message}:\n");
+            Console.WriteLine(result);
+        }
+    }
+}
